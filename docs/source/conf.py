85047ea5b24ed055762003668ed355c69983cc43
kai475 <kai475@163.com>
2016-11-11 18:12:21 +0800

    update docs and refresh index link

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -59,7 +59,7 @@ source_suffix = '.rst'
 # source_encoding = 'utf-8-sig'
 
 # The master toctree document.
-master_doc = 'index'
+master_doc = 'docindex'
 
 # General information about the project.
 project = u'wego'
