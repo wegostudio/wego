2c23c3cfe65ea8f09df398ad8ca0fab93501829d
root <root@ip-172-31-45-40.us-west-2.compute.internal>
2016-11-11 10:30:56 +0000

    typo

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -59,7 +59,7 @@ source_suffix = '.rst'
 # source_encoding = 'utf-8-sig'
 
 # The master toctree document.
-master_doc = 'docindex'
+master_doc = 'index'
 
 # General information about the project.
 project = u'wego'
